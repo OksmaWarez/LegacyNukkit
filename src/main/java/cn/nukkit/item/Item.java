1b018650b6799af87c2e8628e7ba3673e68c59a3
Рауль Вахитов <mrgenga537@gmail.com>
2016-07-10 05:33:29 +0500

    Revert "addEnchantment is more easy now" (#926)

--- src/main/java/cn/nukkit/item/Item.java
+++ src/main/java/cn/nukkit/item/Item.java
@@ -1491,7 +1491,7 @@ public class Item implements Cloneable {
         return null;
     }
 
-    public Item addEnchantment(Enchantment... enchantments) {
+    public void addEnchantment(Enchantment... enchantments) {
         CompoundTag tag;
         if (!this.hasCompoundTag()) {
             tag = new CompoundTag();
@@ -1530,7 +1530,6 @@ public class Item implements Cloneable {
         }
 
         this.setNamedTag(tag);
-        return this;
     }
 
     public Enchantment[] getEnchantments() {
