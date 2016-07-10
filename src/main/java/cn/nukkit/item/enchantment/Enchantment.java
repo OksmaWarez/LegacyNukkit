d7bc26bd750890a8a4707da6b50b45ac2632625a
Pub4Game <Pub4Game@users.noreply.github.com>
2016-07-10 03:38:11 +0300

    Revert "More compact setLevel" because it very buggy
    close #925

--- src/main/java/cn/nukkit/item/enchantment/Enchantment.java
+++ src/main/java/cn/nukkit/item/enchantment/Enchantment.java
@@ -128,15 +128,14 @@ public abstract class Enchantment implements Cloneable {
         return level;
     }
 
-    public Enchantment setLevel(int level) {
+    public void setLevel(int level) {
         this.setLevel(level, true);
-        return this;
     }
 
-    public Enchantment setLevel(int level, boolean safe) {
+    public void setLevel(int level, boolean safe) {
         if (!safe) {
             this.level = level;
-            return this;
+            return;
         }
 
         if (level > this.getMaxLevel()) {
@@ -146,7 +145,6 @@ public abstract class Enchantment implements Cloneable {
         } else {
             this.level = level;
         }
-        return this;
     }
 
     public int getId() {
