5b6a3cffd953a251440a414a5a8805fdb6ba4658
极夜暗芒使 <magicdroidx@gmail.com>
2016-07-28 15:00:15 +0800

    Better way to log event exceptions.

--- src/main/java/cn/nukkit/plugin/PluginManager.java
+++ src/main/java/cn/nukkit/plugin/PluginManager.java
@@ -566,10 +566,7 @@ public class PluginManager {
                     registration.callEvent(event);
                 } catch (Exception e) {
                     this.server.getLogger().critical(this.server.getLanguage().translateString("nukkit.plugin.eventError", new String[]{event.getEventName(), registration.getPlugin().getDescription().getFullName(), e.getMessage(), registration.getListener().getClass().getName()}));
-                    MainLogger logger = this.server.getLogger();
-                    if (logger != null) {
-                        logger.logException(e);
-                    }
+                    this.server.getLogger().logException(e);
                 }
             }
         } catch (IllegalAccessException e) {
