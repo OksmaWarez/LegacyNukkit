071e0833c543333b4cf8801ff61ef0f9309d8a66
madebyoksma <mantaskavaliauskas44@gmail.com>
2026-02-18 18:15:07 +0200

    what

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -3109,7 +3109,7 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                 break;
             case ProtocolInfo.REQUEST_CHUNK_RADIUS_PACKET:
                 RequestChunkRadiusPacket requestChunkRadiusPacket = (RequestChunkRadiusPacket) packet;
-                ChunkRadiusUpdatedPacket chunkRadiusUpdatePacket = new ChunkRadiusUpdatedPacket();
+                RequestChunkRadiusPacket chunkRadiusUpdatePacket = new RequestChunkRadiusPacket();
                 this.chunkRadius = Math.max(5, Math.min(requestChunkRadiusPacket.radius, this.viewDistance));
                 chunkRadiusUpdatePacket.radius = this.chunkRadius;
                 this.dataPacket(chunkRadiusUpdatePacket);
